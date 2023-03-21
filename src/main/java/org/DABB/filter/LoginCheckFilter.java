d16f47dc856303ec17ce450a3f9df8c1d809ea60
DABB <1433683384@qq.com>
2023-03-21 20:39:27 +0800

    Peggie练习1.1

--- src/main/java/org/DABB/filter/LoginCheckFilter.java
+++ src/main/java/org/DABB/filter/LoginCheckFilter.java
@@ -1,24 +1,68 @@
 package org.DABB.filter;
 
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
+import org.DABB.commons.R;
 import org.springframework.util.AntPathMatcher;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter(filterName = "loginCheckFilter",urlPatterns = "/*")
+@WebFilter(filterName = "loginCheckFilter", urlPatterns = "/*")
 @Slf4j
 public class LoginCheckFilter implements Filter {
-    //    路径匹配器
+    //    路径匹配器,支持通配符
     private static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpConstraintElement request = (HttpConstraintElement) servletRequest;
-        HttpConstraintElement response = (HttpConstraintElement) servletResponse;
 
-//        获取本次请求URI
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+//        获取本次请求的URI
+        String requestURI = request.getRequestURI();
+//判断本次请求是否需要处理
 
+        String[] urls = new String[]{
+                "/employee/login",
+                "/employee/logout",
+                "/backend/**",
+                "/front/**"
+        };
+//        判断是否放行
+        boolean check = check(urls, requestURI);
+//        如不需要处理直接放行
+        if (check) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+//        判断登录状态
+        if (request.getSession().getAttribute("employee") != null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+//       如果未登录则返回未登录结果，通过输出流来相应数据
+        response.getWriter().write(JSON.toJSONString(R.error("NOTLOGIN")));
+        log.info("拦截到请求:{}", requestURI);
+    }
+
+    /**
+     * 本次请求是否放行
+     * 路径匹配
+     *
+     * @param requestURI
+     * @return
+     */
+    private boolean check(String[] urls, String requestURI) {
+        for (String url : urls) {
+            boolean match = PATH_MATCHER.match(url, requestURI);
+            if (match) {
+                return true;
+            }
+        }
+        return false;
     }
 }

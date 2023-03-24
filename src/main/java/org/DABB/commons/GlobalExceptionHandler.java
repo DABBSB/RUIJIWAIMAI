a3938bc3cef6c7bb87c2ddb13babb846be3f43e2
DABBSB <96428545+DABBSB@users.noreply.github.com>
2023-03-24 11:12:32 +0800

    Update GlobalExceptionHandler.java

--- src/main/java/org/DABB/commons/GlobalExceptionHandler.java
+++ src/main/java/org/DABB/commons/GlobalExceptionHandler.java
@@ -2,22 +2,21 @@ package org.DABB.commons;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLIntegrityConstraintViolationException;
 
 //全局异常 处理
 
 @RestControllerAdvice(annotations = {RestController.class, Controller.class})
+@ResponseBody
 @Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
-    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException Ex) {
-        log.error(Ex.getMessage());
-        if (Ex.getMessage().contains("Duplicate entry")) {
-            String[] s = Ex.getMessage().split(" ");
+    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException exception) {
+        log.error(exception.getMessage());
+        if (exception.getMessage().contains("Duplicate entry")) {
+            String[] s = exception.getMessage().split(" ");
             return R.error(s[2] + "已存在");
         }
         return R.error("未知错误");
